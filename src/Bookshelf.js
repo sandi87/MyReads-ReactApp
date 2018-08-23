2e0b16b3fd3826818bd61c9b8a1cac3bc029b782
sandi87 <sndrstank@gmail.com>
2018-08-23 19:39:50 +0200

    move content to Book component and Bookshelf

---
+++ src/Bookshelf.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Bookshelf extends Component {
+  render() {
+    return(
+      <div className="bookshelf">
+        <h2 className="bookshelf-title"></h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <li>
+
+            </li>
+          </ol>
+        </div>
+      </div>
+    );
+  }
+};
+export default Bookshelf;
