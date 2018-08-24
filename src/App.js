b75ef4992f3dffbbb35635ed049655e8dbcaa266
sandi87 <sndrstank@gmail.com>
2018-08-24 17:33:17 +0200

    make book display on shelves

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import SearchPage from './SearchPage';
 import MainPage from './MainPage';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    showSearchPage: false
+    books: []
   }
+  {/* use this method to get external data */}
+   componentDidMount() {
+     BooksAPI.getAll().then((books) => {
+       this.setState({ books })
+     })
 
+   }
   render() {
+
     return(
       <div className="app">
-      <MainPage />
+      <MainPage {/* use props to use them on MainPage*/}
+      books={this.state.books}
+      />
       </div>
     )
 };
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react"
-import Book from './Book'
-import Bookshelf from './Bookshelf'
+import React, { Component } from "react";
+import Book from "./Book";
+import * as BooksAPI from "./BooksAPI";
 
 class MainPage extends Component {
   render() {
@@ -10,14 +10,60 @@ class MainPage extends Component {
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-<Bookshelf />
-
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  { {/*display books on proper shelves using filter and map methods
+                     and do it for every shelf*/}
+                    this.props.books .filter(book => book.shelf ===
+                  'currentlyReading') .map( book =>(
+                  <li key={book.id}>
+                    {/* making a list every list item needs a unique key*/}
+                    <Book />
+                  </li>))
+                }
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {
+                    this.props.books
+                    .filter(book => book.shelf === "wantToRead")
+                    .map(book => (
+                      <li key={book.id}>
+                        <Book />
+                      </li>))
+                  }
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                  {
+                    this.props.books
+                  .filter(book => book.shelf === 'read')
+                  .map(book =>(
+                  <li key={book.id}>
+                    <Book />
+                  </li>))
+                }
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="open-search">
           <a onClick={() => this.setState({ showSearchPage: true })}>
             Add a book
           </a>
-          </div>
-      </div>
+        </div>
       </div>
     );
   }
